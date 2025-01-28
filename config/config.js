ce28137920cc32e7a2b265038f4a71130530a85b
anik-44 <anikk3460@gmail.com>
2025-01-29 01:48:10 +0530

    add admin

---
+++ config/config.js
@@ -0,0 +1,30 @@
+import Connect from 'connect-pg-simple'
+import session from 'express-session'
+import prisma from "../db/prisma.js";
+
+export const authenticate = async (email, password) => {
+    console.log(email, password);
+    if (email && password) {
+        const user = await prisma.user.findUnique({where: {email: email}});
+        console.log(user)
+
+        if (!user) {
+            return null;
+        }
+        
+        if (user?.password === password && user?.role === 'Admin') {
+            return Promise.resolve(user)
+        } else {
+            return null;
+        }
+    }
+    return null;
+}
+const ConnectSession = Connect(session)
+export const sessionStore = new ConnectSession({
+    conObject: {
+        connectionString: process.env.DIRECT_URL, ssl: process.env.NODE_ENV === 'production',
+    }, tableName: 'session', createTableIfMissing: true,
+})
+export const COOKIE_SECRET = process.env.COOKIE_PASSWORD
+export const PORT = process.env.PORT || 3000
--- index.js
+++ index.js
@@ -3,21 +3,37 @@ import dotenv from 'dotenv';
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import {admin, buildAdminRouter} from "./admin/setup.js";
+import {PORT} from "./config/config.js";
+
 
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
 
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
+const start = async () => {
+    const app = express();
 
-app.get('/health', (_req, res) => {
-    res.status(200).send('Server running!');
-});
+    app.use(express.json());
+    app.use(cookieParser());
 
+    const appRouter = express.Router();
+    const router = buildAdminRouter(appRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    app.use(admin.options.rootPath, router);
+
+    app.use('/api/auth', authRouter)
+    app.use('/api/user', userRouter)
+
+    app.get('/health', (_req, res) => {
+        res.status(200).send('Server running!');
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+        console.log(`admin path: http://localhost:${PORT}${admin.options.rootPath}`);
+    });
+}
+
+start().catch((err) => {
+    console.log(err);
+    process.exit(1)
+})
