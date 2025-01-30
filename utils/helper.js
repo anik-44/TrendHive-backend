e5dd0a3a6053b0701b553661e2ec43cd1271be22
anik-44 <anikk3460@gmail.com>
2025-01-30 15:32:27 +0530

    add cors header

--- utils/helper.js
+++ utils/helper.js
@@ -16,11 +16,11 @@ export const generateTokens = (userId) => {
 
 export const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
-        httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "strict", maxAge: 15 * 60 * 1000,
+        httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "lax", maxAge: 15 * 60 * 1000,
     });
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true, // prevent XSS attacks, cross site scripting attack
-        secure: process.env.NODE_ENV === "production", sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+        secure: process.env.NODE_ENV === "production", sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 };
